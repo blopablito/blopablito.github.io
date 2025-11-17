7f0592d37451c8722b790b824e40e48a29ecad25
Pablo <pablo.infantes@ucsp.edu.pe>
2025-11-17 12:37:24 -0500

    retorno

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import AppHeader from "./components/AppHeader"; 
+import Navbar from "./components/Navbar";
+import AppHeader from "./components/AppHeader";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import Account from "./pages/Account";
 import About from "./pages/About";
 import Recipe from "./pages/Recipe";
 import Editor from "./pages/Editor";
-import AdminRecipes from "./pages/AdminRecipes";
-
+import AdminRecipes from "./pages/AdminRecipes"; 
 export default function App() {
   return (
     <>
       <AppHeader />
+      <Navbar />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -20,7 +21,7 @@ export default function App() {
           <Route path="/acerca" element={<About />} />
           <Route path="/receta/:id" element={<Recipe />} />
           <Route path="/editor" element={<Editor />} />
-          <Route path="/admin/recetas" element={<AdminRecipes />} />
+          <Route path="/admin/recetas" element={<AdminRecipes />} /> {/* ✅ ruta admin */}
           <Route path="*" element={<div className="container">Página no encontrada</div>} />
         </Routes>
       </main>
